d2628cda0a642583664861fae6290710d87cfc3a
申祥 <shenxiang03@maoyan.com>
2019-06-03 13:11:13 +0800

    d

--- src/main/java/com/leetcode/algorithms/arraysearch/RandomArraySearch.java
+++ src/main/java/com/leetcode/algorithms/arraysearch/RandomArraySearch.java
@@ -1,24 +1,51 @@
 package com.leetcode.algorithms.arraysearch;
 
 
+import com.google.common.primitives.Ints;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author myshenx
+ */
 public class RandomArraySearch {
 
-    private int[] BalenceRandomSerach(int[] arr,int n){
-        //[1,2,3,4,5,6,7]
-        int len=arr.length;
-
-        int[] t=new int[10];
-        for (int i = 0; i < arr.length; i++) {
-            if(i%(n+1)==0){
-                System.out.println(i);
-            }
+    private Integer[] BalenceRandomSerach(int[] arr, int n){
+        if (n>arr.length||n<=0) {
+            return null;
         }
-        return t;
+        List<Integer> list=new ArrayList<>();
+        int ff;
+        int fornum=0;
+       if (n<=arr.length/2) {
+           fornum=n;
+            ff = (int) Math.ceil(arr.length / (n + 1.0));
+       }else {
+           fornum=(arr.length-n);
+           ff= (int) Math.ceil(arr.length /(fornum+1.0));
+           list=new ArrayList<>(Ints.asList(arr));
+       }
+        int index=ff-1;
+       for (int i=0;i<fornum;i++){
+           if (n<=arr.length/2){
+               list.add(arr[index]);
+           }else {
+               list.remove(index);
+           }
+           index+=ff;
+       }
+
+        return list.toArray(new Integer[n]);
     }
+
 
     public static void main(String[] args) {
         RandomArraySearch randomArraySearch=new RandomArraySearch();
         int[] arr=new int[]{3,2,5,6,7,3,8};
-        randomArraySearch.BalenceRandomSerach(arr,3);
+        Integer[] result = randomArraySearch.BalenceRandomSerach(arr,1);
+        for (int i : result) {
+            System.out.println(i);
+        }
     }
 }
